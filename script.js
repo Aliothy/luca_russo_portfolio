ec128fe0267c5941b44fe7e3a7f2786902db7eba
Aliothy <luca.russo2000@gmail.com>
2025-01-25 11:10:27 -0600

    January 25,1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// change the section
 document.addEventListener("DOMContentLoaded", () => {
     const links = document.querySelectorAll(".nav-link");
     const sections = document.querySelectorAll(".section");
@@ -17,4 +18,36 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+// // JavaScript to Handle Modal
+// document.addEventListener('DOMContentLoaded', () => {
+//     const modalDescription = document.getElementById('modal content');
+//     const closeModal = document.getElementById('close modal');
+  
+//     // Add click event to all clickable images
+//     document.querySelectorAll('.clickable').forEach(image => {
+//       image.addEventListener('click', () => {
+//         const src = image.getAttribute('src');
+//         const description = image.getAttribute('data-description');
+  
+//         modalImage.src = src;
+//         modalDescription.textContent = description;
+  
+//         // Show the modal
+//         modal.style.display = 'block';
+//       });
+//     });
+  
+//     // Close the modal when the close button is clicked
+//     closeModal.addEventListener('click', () => {
+//       modal.style.display = 'none';
+//     });
+  
+//     // Close the modal when clicking outside of the modal content
+//     window.addEventListener('click', event => {
+//       if (event.target === modal) {
+//         modal.style.display = 'none';
+//       }
+//     });
+//   });
 
